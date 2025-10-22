c2c7dd71555f5543396efe5dcc47e74b16526434
Fassam Khan <muhammadfassam465@gmail.com>
2025-10-22 10:44:56 +0500

    changes in api

--- components/mycomponent/Feature.jsx
+++ components/mycomponent/Feature.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import ProductCard from './ProductCard'
+"use client";
+import React, { useState, useEffect } from "react";
+import ProductCard from "./ProductCard";
+
 const Feature = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data))
+      .catch((err) => console.error("Error:", err));
+  }, []);
+
   return (
-    <div className='mt-12 p-2'>
-        <h2 className='text-2xl font-semibold mb-6 '>Today's big deals</h2>
-        <div className='grid md:grid-cols-4 grid-cols-2 gap-2  overflow-auto'>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
+    <div className="mt-12 p-2">
+      <h2 className="text-2xl font-semibold mb-6">Today's Big Deals</h2>
 
-        </div>
-      
+      <div className="grid md:grid-cols-4 grid-cols-2 gap-4 overflow-auto">
+        {products.map((item) => (
+          <ProductCard key={item.id} product={item} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Feature
+export default Feature;
--- components/mycomponent/ProductCard.jsx
+++ components/mycomponent/ProductCard.jsx
@@ -1,12 +1,26 @@
+"use client"
 import React from 'react'
+import { useEffect, useState } from 'react';
 
-const ProductCard = () => {
+
+
+
+
+
+const ProductCard = ({product}) => {
+  
+  
+
   return (
     <div className='outline outline-gray-300 md:w-[260px] rounded-2xl hover:shadow-xl cursor-pointer relative group  focus:shadow-2xl'>
-        <div className='bg-[url(/Images/hero.webp)] bg-cover md:h-[270px] h-[200px] rounded-t-2xl p-2'></div>
+        <div className='bg-cover md:h-[270px] h-[200px] rounded-t-2xl p-2'><img
+    src={product.image || product.images?.[0]}
+    alt={product.title}
+    className="w-full h-64 object-cover rounded-t-2xl"
+  /></div>
         <div className='p-2'>
-            <p>Title</p>
-            <p className='text-xl text-orange-400'>PKR 300</p>
+            <p>{product.title}</p>
+            <p className='text-xl text-orange-400'>{product.price}</p>
         </div>
         <span className= 'hidden group-hover:inline transition-all group-focus:inline bg-white rounded-full p-2 absolute top-2 right-2'><img src="heart.svg" alt="" width={16} /></span>
 
