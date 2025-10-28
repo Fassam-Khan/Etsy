30d56cca8692eb40cb0154efe01520c6adf431d1
Fassam Khan <muhammadfassam465@gmail.com>
2025-10-28 23:07:36 +0500

    Update BlogList layout for better responsiveness and fix typo in button label

--- components/mycomponent/BlogList.jsx
+++ components/mycomponent/BlogList.jsx
@@ -5,7 +5,7 @@ import BlogItem from './BlogItem'
 const BlogList = () => {
   return (
     <div>
-        <div className='flex gap-6 justify-center  mt-10'>
+        <div className='flex gap-6 justify-center flex-wrap  mt-10'>
             <button className='cursor-pointer  bg-gray-300 py-1.5 px-4 rounded-4xl'>All</button>
             <button className='cursor-pointer  bg-gray-300 py-1.5 px-4 rounded-4xl'>Electronic</button>
             <button className='cursor-pointer  bg-gray-300 py-1.5 px-4 rounded-4xl'>Web Dovelopment</button>
@@ -13,7 +13,7 @@ const BlogList = () => {
             <button className='cursor-pointer  bg-gray-300 py-1.5 px-4 rounded-4xl'>Digital Marketing</button>
 
         </div>
-        <div className='grid grid-cols-4 mt-12'>
+        <div className='grid md:grid-cols-4 mt-12 grid-cols-1'>
             {blog.map((item,index)=>{
                 return <BlogItem key={index} image={item.image_url} title={item.blog_title} description={item.description}/>
             })}
