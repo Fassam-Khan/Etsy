efedcebdde974c7461f63fa31c40954bf23742c0
Fassam Khan <muhammadfassam465@gmail.com>
2025-11-03 00:39:56 +0500

    Add blog data fetching and display functionality; update dependencies in package.json and package-lock.json

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -1,12 +1,41 @@
+'use client'
+import { useState ,useEffect } from 'react';
 import React from 'react'
+import { useParams } from 'next/navigation'
+import { blog } from '@/data/blogdata';
+import Image from 'next/image';
+export default function Page() {
+  const params = useParams()
+  const [data, setdata] = useState(null)
+  const fetchBlogData = () => {
+    for (let i = 0; i < blog.length; i++) {
+      if (Number(params.id) === blog[i].blog_id) {
+        setdata(blog[i]);
+        console.log(blog[i]);
+        break;
+      }
+    }
+  }
+  useEffect(() => {
+    fetchBlogData();
+  
+  }, [])
 
-const page = ({params}) => {
-  return (
-    <div>
-      {params.id}
+
+  return (data?<>
+    <div className='max-w-[1280px] p-6 m-auto'>
+      <div>
+        <div className='border '>
+        <Image src={data.image_url} width={250} height={300} alt={data.blog_title} className='object-fill' />
+
+        </div>
+        
+        <h1>{data.blog_title}</h1>
+        <p>{data.description}</p>
+      </div>
       
     </div>
+    </>:<>
+    <div>Loading.....</div></>
   )
 }
-
-export default page
