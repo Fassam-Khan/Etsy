68d40ccd2cd24d98115274f1ecf7153c3f83b83f
Fassam-Khan <muhammadfassam465@gmail.com>
2025-11-05 14:20:54 +0500

    apaasjalsa

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,28 +1,9 @@
-import Link from "next/link";
-import { productData } from "@/data/product";
-export default function ProductsPage() {
+import React from 'react'
+
+const page = () => {
   return (
-    <div className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-      {products.map((product) => (
-        <div
-          key={product.id}
-          className="border rounded-xl shadow p-4 text-center hover:shadow-lg transition"
-        >
-          <img
-            src={product.image}
-            alt={product.name}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
-            className="w-full h-48 object-cover rounded-lg"
-          />
-          <h2 className="text-lg font-semibold mt-3">{product.name}</h2>
-          <p className="text-gray-700 mt-1">Rs. {product.price}</p>
-          <Link
-            href={`/products/${product.id}`}
-            className="inline-block mt-3 bg-red-600 text-white px-4 py-2 rounded-lg hover:bg-red-700"
-          >
-            View Details
-          </Link>
-        </div>
-      ))}
-    </div>
-  );
+    <div>this is shop  product page</div>
+  )
 }
+
+export default page
