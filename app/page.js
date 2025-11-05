89a15fabd72e63c9de9d5884f18e0495e472240c
Fassam-Khan <muhammadfassam465@gmail.com>
2025-11-05 14:18:20 +0500

    dshdsds

--- app/page.js
+++ app/page.js
@@ -1,9 +1,6 @@
 import Image from "next/image";
 import Hero from "@/components/mycomponent/Hero";
-import Feature from "@/components/mycomponent/Feature";
-import GiftCard from "@/components/mycomponent/GiftCard"
 import Link from "next/link";
-import ProductCard from "@/components/mycomponent/ProductCard";
 import ProductList from "@/components/mycomponent/ProductList";
 
 export default function Home() {
